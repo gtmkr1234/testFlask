af74b21a18438e3f8c0284c833cd3bf5a2db26ef
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-29 09:10:03 +0530

    main code setup

--- app.py
+++ app.py
@@ -9,4 +9,22 @@ app=None
 
 def create_app:
     app = Flask(__name__,template_folder="templates")
-    if(os.getenv('ENV',"development"))
+    if os.getenv('ENV', "development")== "production":
+        raise Exception("Currently no production config is setup")
+    else:
+        print("Starting local development")
+        app.config.from_object(LocalDevelopmentConfig)
+    db.init_app(app)
+    app.app_context().push()
+    return app
+
+
+app = create_app()
+
+
+#Importing all controllers so they are loaded
+from application.controllers import *
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=5050)
