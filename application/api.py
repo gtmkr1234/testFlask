29a944ec88d1fe6b3e6eea8fd5c2bfcb117e8fe2
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-30 01:28:26 +0530

    Bug fixes while API calls

--- application/api.py
+++ application/api.py
@@ -3,35 +3,37 @@ from application.database import db
 from application.models import User
 from flask_restful import fields, marshal_with
 
-
-output_fields={
-    "user_id" : fields.Integer,
-    "username" : fields.String,
-    "email" : fields.String
+output_fields = {
+    "user_id": fields.Integer,
+    "username": fields.String,
+    "email": fields.String
 }
 
+
 class UserAPI(Resource):
+    @marshal_with(output_fields)
     def get(self, username):
-        #getting the username
+        # getting the username
         print("GET method in User API: ", username)
 
-        #querying the database
-        user = db.session.query(User).filter(User.username==username).first()
+        # querying the database
+        user = db.session.query(User).filter(User.username == username).first()
         if user:
-            #return a valid json
+            # return a valid json
             return user
 
         else:
-            #return 404 error
+            # return 404 error
             return '', 404
 
     def put(self, username):
         print("PUT username", username)
         return {"username": username}
+
     def delete(self, username):
         print("DELETE username", username)
         return {"username": username, "action": "DELETE"}
+
     def post(self):
         print("POST")
         return {"action": "POST"}
-
