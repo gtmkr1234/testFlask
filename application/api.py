7d667a5723c808c98b7644ab53e5533a8868d8a3
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-30 00:22:09 +0530

    API setup

---
+++ application/api.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+
+
+class UserAPI(Resource):
+    def get(self):
+        pass
+    def put(self):
+        pass
+    def delete(self):
+        pass
+    def post(self):
+        pass
+
