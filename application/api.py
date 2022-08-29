5c144e14ff671639af47d643c7dfa6434b0cfc8d
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-30 01:00:06 +0530

    API setup

--- application/api.py
+++ application/api.py
@@ -1,7 +1,14 @@
 from flask_restful import Resource
 from application.database import db
 from application.models import User
+from flask_restful import fields, marshal_with
 
+
+output_fields={
+    "user_id" : fields.Integer,
+    "username" : fields.String,
+    "email" : fields.String
+}
 
 class UserAPI(Resource):
     def get(self, username):
@@ -12,7 +19,7 @@ class UserAPI(Resource):
         user = db.session.query(User).filter(User.username==username).first()
         if user:
             #return a valid json
-            return {"user_id": user.user_id, "username": user.username, "email":user.email}
+            return user
 
         else:
             #return 404 error
