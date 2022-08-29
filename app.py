744116228b5ad8bf120d83733595ef37947ce226
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-29 09:02:56 +0530

    app config

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ from flask import Flask
 from application import config
 from application.config import LocalDevelopmentConfig
 from applicatin.database import db
+
+
+app=None
+
+def create_app:
+    app = Flask(__name__,template_folder="templates")
+    if(os.getenv('ENV',"development"))
