f6eda7a2272b4e514c8c3d53f5a402a709aa8808
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-30 00:28:38 +0530

    API setup

--- application/api.py
+++ application/api.py
@@ -2,11 +2,11 @@ from flask_restful import Resource
 
 
 class UserAPI(Resource):
-    def get(self):
+    def get(self, username):
         pass
-    def put(self):
+    def put(self, username):
         pass
-    def delete(self):
+    def delete(self, username):
         pass
     def post(self):
         pass
