25a65313244b6cc7a82ad79aad0903174659c2b3
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-30 00:54:00 +0530

    API setup

--- application/api.py
+++ application/api.py
@@ -1,10 +1,23 @@
 from flask_restful import Resource
+from application.database import db
+from application.models import User
 
 
 class UserAPI(Resource):
     def get(self, username):
+        #getting the username
+        print("GET method in User API: ", username)
 
-        return {"username": username}
+        #querying the database
+        user = db.session.query(User).filter(User.username==username).first()
+        if user:
+            #return a valid json
+            return {"user_id": user.user_id, "username": user.username, "email":user.email}
+
+        else:
+            #return 404 error
+            return '', 404
+
     def put(self, username):
         print("PUT username", username)
         return {"username": username}
